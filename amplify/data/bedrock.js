d66060f29ae8975d37ffdf032a0f7ed171c0607b
Shiv Lal Jat <shiv-lal.jat@capgemini.com>
2025-07-30 17:01:43 +0530

    model related details corrected

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -6,14 +6,14 @@ export function request(ctx) {
   
     // Return the request configuration
     return {
-      resourcePath: `/model/anthropic.claude-3-sonnet-20240229-v1:0/invoke`,
+      resourcePath: `/anthropic.claude-3-5-sonnet-20240620-v1:0/invoke`,
       method: "POST",
       params: {
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          anthropic_version: "bedrock-2023-05-31",
+          anthropic_version: "bedrock-2024-06-20",
           max_tokens: 1000,
           messages: [
             {
